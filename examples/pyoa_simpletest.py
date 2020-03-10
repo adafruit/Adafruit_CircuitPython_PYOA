76769dc68add41e5218be9076a8001e8c1b8d1e5
dherrada <dylan.herrada@gmail.com>
2020-03-09 20:14:03 -0400

    Ran black, updated to pylint 2.x

--- examples/pyoa_simpletest.py
+++ examples/pyoa_simpletest.py
@@ -8,14 +8,14 @@ try:
     sdcard = adafruit_sdcard.SDCard(board.SPI(), digitalio.DigitalInOut(board.SD_CS))
     vfs = storage.VfsFat(sdcard)
     storage.mount(vfs, "/sd")
-    print("SD card found") # no biggie
+    print("SD card found")  # no biggie
 except OSError:
-    print("No SD card found") # no biggie
+    print("No SD card found")  # no biggie
 
 gfx = PYOA_Graphics()
 
 gfx.load_game("/cyoa")
-current_card = 0   # start with first card
+current_card = 0  # start with first card
 
 while True:
     print("Current card:", current_card)
