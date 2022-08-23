c3157659e024388e4331cf2d301ce2ebe810eb2f
Alec Delaney <tekktik@gmail.com>
2022-08-23 17:26:22 -0400

    Use year duration range for copyright attribution

--- docs/conf.py
+++ docs/conf.py
@@ -54,8 +54,14 @@ master_doc = "index"
 
 # General information about the project.
 project = "Adafruit PYOA Library"
+creation_year = "2019"
 current_year = str(datetime.datetime.now().year)
-copyright = current_year + " Adafruit"
+year_duration = (
+    current_year
+    if current_year == creation_year
+    else creation_year + " - " + current_year
+)
+copyright = year_duration + " Adafruit"
 author = "Adafruit"
 
 # The version info for the project you're documenting, acts as replacement for
